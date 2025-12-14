b37932ef685f2ab49f2dbd5d2b2ec8a20e1d8ea1
Fakhruddin Hashmi <109304853+hashmi846003@users.noreply.github.com>
2025-12-14 18:27:21 +0530

    ls

--- ls.c
+++ ls.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
+#include <sys/stat.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
     DIR *dp;
     struct dirent *dirp;
+    struct stat statbuf;
+    char path[1024];
 
     if (argc != 2) {
-        fprintf(stderr, "usage: %s directory_name\n", argv[0]);
+        fprintf(stderr, "Usage: %s directory_name\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    if ((dp = opendir(argv[1])) == NULL) {
-        perror("can't open directory");
+    dp = opendir(argv[1]);
+    if (dp == NULL) {
+        perror("Can't open directory");
         exit(EXIT_FAILURE);
     }
 
     while ((dirp = readdir(dp)) != NULL) {
-        printf("%s\n", dirp->d_name);
+        snprintf(path, sizeof(path), "%s/%s", argv[1], dirp->d_name);
+        if (stat(path, &statbuf) == 0 && S_ISDIR(statbuf.st_mode)) {
+            printf("%s\n", dirp->d_name);
+        }
     }
 
     closedir(dp);
